44de8b69e51d4680ac11df5e9d2603762c05a498
Aksh <aksh.patel0824@gmail.com>
2024-06-25 08:49:31 -0400

    Add new feature

--- selectdemo.js
+++ selectdemo.js
@@ -1,8 +1,7 @@
 function setselected() {
     let selectOption = document.getElementById("01");
     selectOption.value = "Browser";
-    let selectOption2 = document.getElementById("02");
-    selectOption2.value = "Edge";
+    
 }
 
 function getselected() {
